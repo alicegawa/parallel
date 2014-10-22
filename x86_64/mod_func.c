0546961caa114ffc7c05c985f2f633b8be4b3648
alicegawa <fukuda@brain.imi.i.u-tokyo.ac.jp>
2014-10-22 19:37:14 +0900

    try to change the serial codes to parallel ones, but they are left to test.

---
+++ x86_64/mod_func.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "hocdec.h"
+extern int nrnmpi_myid;
+extern int nrn_nobanner_;
+
+extern void _gamma_reg(void);
+extern void _stdp_reg(void);
+
+void modl_reg(){
+  if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+    fprintf(stderr, "Additional mechanisms from files\n");
+
+    fprintf(stderr," mod/gamma.mod");
+    fprintf(stderr," mod/stdp.mod");
+    fprintf(stderr, "\n");
+  }
+  _gamma_reg();
+  _stdp_reg();
+}
---
+++ x86_64/stdp.mod
@@ -0,0 +1,1 @@
+../mod/stdp.mod
