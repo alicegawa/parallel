fa462026b329e94f74d714ffd3a601236cd834f1
alicegawa <fukuda@brain.imi.i.u-tokyo.ac.jp>
2014-12-25 16:18:42 +0900

    supervise oakleaf's change

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+#
+#PJM --rsc-list "rscgrp=short"
+#PJM --rsc-list "node=10"
+#PJM --rsc-list "elapse=00:40:00"
+#PJM --mpi "proc=160"
+#PJM -s
+#PJM -m "e"
+#
+
+
+NRNIV="../../neuron_kplus/specials/sparc64/special -mpi"
+HOC_NAME="./network.hoc"
+
+LPG="lpgparm -t 4MB -s 4MB -d 4MB -p 4MB"
+MPIEXEC="mpiexec -mca mpi_print_stats 1"
+PROF=""
+echo "${PROF} ${MPIEXEC} ${LPG} ${NRNIV} ${HOC_NAME}"
+time ${PROG} ${MPIEXEC} ${LPG} ${NRNIV} ${HOC_NAME}
+
+sync
